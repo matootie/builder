431451f45326c305b731b6daee11eb05d3b7de23
Mateja Lasan <matootie@icloud.com>
2022-01-20 01:21:33 -0500

    Base setup for the web application

---
+++ web/pages/index.tsx
@@ -0,0 +1,15 @@
+import type { NextPage } from "next"
+import Head from "next/head"
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <Head>
+        <title>Builder</title>
+      </Head>
+      <h1 className="text-7xl font-bold">Hello, Builder!</h1>
+    </div>
+  )
+}
+
+export default Home
