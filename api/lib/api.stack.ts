f67be433b991a32b4643e83f10828c931a322dc4
Mateja Lasan <m.vuk.lasan@icloud.com>
2022-03-20 19:17:50 -0400

    Fix missing secret in deployment

--- api/lib/api.stack.ts
+++ api/lib/api.stack.ts
@@ -3,10 +3,11 @@
  */
 
 // External imports.
-import { Stack, StackProps } from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-import { Function, Runtime, Code } from "aws-cdk-lib/aws-lambda";
+import { Stack, StackProps } from "aws-cdk-lib"
+import { Construct } from "constructs"
+import { Function, Runtime, Code } from "aws-cdk-lib/aws-lambda"
 import { LambdaRestApi } from "aws-cdk-lib/aws-apigateway"
+import { Secret } from "aws-cdk-lib/aws-secretsmanager"
 
 /**
  * The App stack.
@@ -16,13 +17,23 @@ interface AppStackProps extends StackProps {
 }
 export class ApiStack extends Stack {
   constructor(scope: Construct, id: string, props?: AppStackProps) {
-    super(scope, id, props);
+    super(scope, id, props)
+
+    // The Redis connection secrets.
+    const redisSecrets = Secret.fromSecretNameV2(
+      this,
+      "RedisSecret",
+      "Builder/Redis",
+    )
 
     // The AWS Lambda function.
     const handler = new Function(this, "Function", {
       runtime: Runtime.NODEJS_14_X,
       code: Code.fromAsset(props?.codeZipLocation ?? "out/function.zip"),
       handler: "handler.handler",
+      environment: {
+        REDIS_URL: redisSecrets.secretValueFromJson("redisUrl").toString(),
+      },
     })
 
     // The AWS API Gateway for the function.
