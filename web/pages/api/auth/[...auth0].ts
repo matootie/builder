68cb2ae65defe6057ffdcabcb4e14bb1504f9f2f
Mateja Lasan <m.vuk.lasan@icloud.com>
2022-03-22 15:12:28 -0400

    Force Discord connection for login

--- web/pages/api/auth/[...auth0].ts
+++ web/pages/api/auth/[...auth0].ts
@@ -1,3 +1,12 @@
+import { handleLogin } from "@auth0/nextjs-auth0"
 import { auth0 } from "@utils/auth0"
 
-export default auth0.handleAuth()
+export default auth0.handleAuth({
+  async login(req, res) {
+    await handleLogin(req, res, {
+      authorizationParams: {
+        connection: "discord",
+      },
+    })
+  },
+})
