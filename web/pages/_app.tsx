bd6f8b4e5dd2158efc8169bbe7e4a3f7855404ed
Mateja Lasan <m.vuk.lasan@icloud.com>
2022-04-07 00:26:42 -0400

    Drop admin in website, redirect to admin SPA
    Now that the admin panel is being developed as it's own application,
    the admin functionality in the website is no longer needed. For
    convenience, redirect requests to /admin to the admin panel deployment
    on the other subdomain.

--- web/pages/_app.tsx
+++ web/pages/_app.tsx
@@ -1,26 +1,9 @@
 import type { AppProps } from "next/app"
 
-import { NextPage } from "next"
-import { ReactElement, ReactNode } from "react"
-import { UserProvider } from "@auth0/nextjs-auth0"
-
 import "@styles/globals.css"
 
-type NextPageWithLayout = NextPage & {
-  getLayout?: (page: ReactElement) => ReactNode
+function Website({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />
 }
 
-type AppPropsWithLayout = AppProps & {
-  Component: NextPageWithLayout
-}
-
-function MyApp({
-  Component,
-  pageProps: { session, ...pageProps },
-}: AppPropsWithLayout) {
-  const getLayout = Component.getLayout ?? ((page) => page)
-  const page = getLayout(<Component {...pageProps} />)
-  return <UserProvider>{page}</UserProvider>
-}
-
-export default MyApp
+export default Website
